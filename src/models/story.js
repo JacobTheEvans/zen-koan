46ced3346f8d4cfbe1e9dc57b164536aa1e07189
Jacob <jakegod14@gmail.com>
2016-08-03 16:27:01 -0600

    Change build

--- src/models/story.js
+++ src/models/story.js
@@ -1,4 +1,15 @@
-import mongoose from "mongoose";
-let Schema = mongoose.Schema;
+import jugglingdb from "jugglingdb";
+let Schema = jugglingdb.Schema;
+let schema  = new Schema("postgres", {
+  database: "scraper",
+  username: "postgres"
+  host: "localhost",
+  password: "scraper",
+});
 
-let U
+let Story = schema.define({
+  title: {type: "String", length: 1000, index: true},
+  text: {type: "Date", length: 10500}
+});
+
+export default Story;
--- src/server/app.js
+++ src/server/app.js
@@ -1,3 +1,27 @@
 import express from "express";
 import bodyParser from "body-parser";
 import path from "require";
+import model from "./models/story.js";
+
+//base app
+let app = express();
+
+//setup JSON requests
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.json());
+
+//setup public facing files
+app.use(express.static(path.join(__dirname + "/public")));
+app.set("views", __dirname + "/public/views");
+
+//setup view engine
+app.engine("html", require("ejs").renderFile);
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res) {
+  res.render("index.html");
+});
+
+app.listen(8080);
+
+export default app;
