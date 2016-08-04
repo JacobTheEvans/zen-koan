bf935e979621d7d6cf6f532b7efadd425de9d728
Jacob <jakegod14@gmail.com>
2016-08-04 16:58:53 -0600

    Add navbar

--- src/public/components/navbar.jsx
+++ src/public/components/navbar.jsx
@@ -2,9 +2,20 @@ import React from "react";
 
 class Navbar extends React.Component {
   render() {
+    var navLinks = this.props.navItems.map(function(item) {
+      return (
+      <li key={item.name} className="nav-item"><a className="nav-link" href={item.link}>{item.name}</a></li>
+      )
+    });
     return (
       <div className="navbar">
-        <div className="logo-image-cont"><img src="/images/logo.png" className="logo-image" /> </div>
+        <div className="logo-image-cont"><img src="/images/logo.png" className="logo-image" /></div>
+        <div className="logo-text-cont"><div className="logo-text">{this.props.logoText}</div></div>
+        <div className="nav-link-bar">
+          <ul className="nav-links">
+            {navLinks}
+          </ul>
+        </div>
       </div>
     );
   }
--- src/public/components/wrapper.jsx
+++ src/public/components/wrapper.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import Navbar from "./navbar.jsx";
 
+var navItems = [
+  {"name": "About", "link": "/about"},
+  {"name": "Stories", "link": "/stories"}
+]
+
 class Wrapper extends React.Component {
   render() {
     return (
       <div className="wrapper">
-        <Navbar />
+        <Navbar logoText="Zen Koan" navItems={navItems}/>
       </div>
     )
   }
