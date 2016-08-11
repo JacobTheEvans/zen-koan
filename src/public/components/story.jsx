4842f60640bc88be0e6702568f7b0c441dd95b31
Jacob <jakegod14@gmail.com>
2016-08-11 08:45:12 -0600

    Update story.jsx for better use

--- src/public/components/story.jsx
+++ src/public/components/story.jsx
@@ -2,6 +2,10 @@ import React from "react";
 import BackButton from "./back-button.jsx";
 
 class Story extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
   render() {
     return (
       <div className="story-section">
